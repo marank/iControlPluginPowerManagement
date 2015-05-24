c7ac93631b68ecffbf512e87bf8f48b416a85b63
Matthias Rank <mail@m-rank.de>
2015-05-24 02:55:48 +0200

    Only react to "pwrmngmt", added config file support

--- iControlPluginPowerManagement/iControlPluginPowerManagement.cs
+++ iControlPluginPowerManagement/iControlPluginPowerManagement.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using iControlPluginInterface;
 
 using System.Runtime.InteropServices;
 
 namespace iControlPluginPowerManagement {
-    public class iControlPlugin : iControlPluginInterface.IiControlPlugin {
+    public class iControlPlugin : IiControlPlugin {
 
         [DllImport("user32")]
         public static extern void LockWorkStation();
@@ -31,40 +33,55 @@ namespace iControlPluginPowerManagement {
         }
 
         public bool Init() {
+            string configFile = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "plugins", "iControlPluginPowerManagement.config");
+            if (System.IO.File.Exists(configFile)) {
+                Dictionary<string, string> settings = pluginHost.DeserializeJSON(configFile);
+                bool value;
+                if (settings.ContainsKey("enabled") && Boolean.TryParse(settings["enabled"], out value) && value == false) {
+                    pluginHost.Log("Plugin disabled", this);
+                    return false;
+                }
+            }
             return true;
         }
 
 
         public void Handle(string[] commands, string ip) {
-            switch (commands[0]) {
-                case "shutdown":
-                    pluginHost.Log("Shutting down", this);
-                    System.Diagnostics.Process.Start("shutdown.exe", "-s -f -t 0");
-                    break;
+            if (commands[0] == "pwrmngmt") {
+                switch (commands[1]) {
+                    case "shutdown":
+                        pluginHost.Log("Shutting down", this);
+                        System.Diagnostics.Process.Start("shutdown.exe", "-s -f -t 0");
+                        break;
 
-                case "restart":
-                    System.Diagnostics.Process.Start("shutdown.exe", "-r -f -t 0");
-                    break;
+                    case "restart":
+                        pluginHost.Log("Restarting", this);
+                        System.Diagnostics.Process.Start("shutdown.exe", "-r -f -t 0");
+                        break;
 
-                case "logoff":
-                    System.Diagnostics.Process.Start("shutdown.exe", "-l -f -t 0");
-                    break;
+                    case "logoff":
+                        pluginHost.Log("Logging of the current user", this);
+                        System.Diagnostics.Process.Start("shutdown.exe", "-l -f -t 0");
+                        break;
 
-                case "sperren":
-                    LockWorkStation();
-                    break;
+                    case "lock":
+                        pluginHost.Log("Lock workstation", this);
+                        LockWorkStation();
+                        break;
 
-                case "hibernate":
-                    System.Windows.Forms.Application.SetSuspendState(System.Windows.Forms.PowerState.Hibernate, true, true);
-                    break;
+                    case "hibernate":
+                        pluginHost.Log("Hibernating", this);
+                        System.Windows.Forms.Application.SetSuspendState(System.Windows.Forms.PowerState.Hibernate, true, true);
+                        break;
 
-                case "standby":
-                case "stand-by":
-                    System.Windows.Forms.Application.SetSuspendState(System.Windows.Forms.PowerState.Suspend, true, true);
-                    break;
+                    case "standby":
+                        pluginHost.Log("Going to stand by", this);
+                        System.Windows.Forms.Application.SetSuspendState(System.Windows.Forms.PowerState.Suspend, true, true);
+                        break;
 
-                default:
-                    break;
+                    default:
+                        break;
+                }
             }
         }
     }
